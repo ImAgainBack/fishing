14e1f63ef9752ae5268f33bbefb7dfdef9db0518
ImAgainBack <artsmogunov@gmail.com>
2022-12-05 21:24:40 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,6 @@
 #----------------------INITIALISATION----------------------#
 from pyautogui import * 
 import pyautogui as pg
-#include functions.py
 import time 
 import random
 import win32api, win32con
@@ -20,64 +19,53 @@ x_player, y_player = 958, 515
 def findlog():
     i=0
     logs = np.empty((0,2),int)
-    for log in pg.locateAllOnScreen("img/log.png", confidence=0.70):
-        
-        #x_new,y_new = x_map - log.left, y_map- log.top
-        logs = np.append(logs, np.array([[log.left, log.top]]),axis=0)
-
-    return logs
+    for log in pg.locateAllOnScreen("img/log.png", confidence=0.70): #hardcoded img 
+        logs = np.append(logs, np.array([[log.left, log.top]]),axis=0) #putting all logs found in a minimap 2d list with
+                                                                        # 2col and inf rows
+    return logs 
 
 
-def draw_rectangle(log):
+def draw_rectangle(log): #just drawing rectangles to test if finds what you need
     screenshot = pg.screenshot() #region=(1595,40,300,250)
     screenshot = cv2.cvtColor(np.array(screenshot),cv2.COLOR_RGB2BGR)
-    cv2.rectangle(
+    cv2.rectangle(  #rectangle function in cv2
     screenshot,
-    (log.left, log.top),
-    (log.left + log.width, log.top + log.height),
-    (0, 0, 255),
-    2)
+    (log.left, log.top), #top left coords of your "log" picture
+    (log.left + log.width, log.top + log.height), #bottom right coords of your "log" picture
+    (0, 0, 255), #color
+    2) #thickness
 
-def distance(logs):
+def distance(logs): #computes and finds the closest "log" to your caracter
     line=0
-    total_distance1=999
-    for row in logs:
+    total_distance1=999 #random high value
+    for row in logs: #loop to compare with 2d array given by findlog() function
         x_new= x_map- logs[line][0]
         y_new= y_map-logs[line][1]
-        total_distance= abs(x_new)+ abs(y_new)
+        total_distance= abs(x_new)+ abs(y_new) #abs() function gives absolute value
         if total_distance<total_distance1:
             total_distance1=total_distance
-            x_tomove, y_tomove = x_new, y_new
+            x_tomove, y_tomove = x_new, y_new #returning values x and y coords the closest to your caracter
         line+=1
     return x_tomove,y_tomove
 
-def giga_loop():
-    p= random.uniform(0.5,1.1)
-    hc = HumanClicker()
-    waiting_secs=0
+def giga_loop(): #main loop
+    p= random.uniform(0.5,1.1) #random value generator between 0.5 and 1.1 
+    hc = HumanClicker() #library
     while True:
-        coords = pg.locateCenterOnScreen("img/mushroom.png", confidence=0.7)
-        #print(coords)
+        coords = pg.locateCenterOnScreen("img/mushroom.png", confidence=0.7)  #finding text of the tree, if finds, going to click and chop it
         logs = findlog()
-        travel_x, travel_y = distance(logs)[0], distance(logs)[1]
-        #print("etap1")
-        if coords != None:
-            hc.move((coords),p)
-            pg.leftClick()
-            sleep(6)
-            wait= pg.locateOnScreen("img/wait.png", confidence=0.8)
-            #print(wait, "11111111111111111")
-            #print("etap2")
-            while wait != None:
-                #print("in while loop")
+        travel_x, travel_y = distance(logs)[0], distance(logs)[1]  #taking x and y coords of the closest log
+        if coords != None:  #if finds a tree (mushroom)
+            hc.move((coords),p) #moves the cursor to it with random time p, between 0.5 and 1.1 
+            pg.leftClick() #left click
+            sleep(6) #waits 6s for the caracter to move
+            wait= pg.locateOnScreen("img/wait.png", confidence=0.8) #searching for a special pattern "wait"
+            while wait != None: #if finds "wait" just waits
                 sleep(1)
-                #print("waiting... ", waiting_secs)
-                waiting_secs+=1
-                wait= pg.locateOnScreen("img/wait.png", confidence=0.8)
-                #print(wait)
-
+                wait= pg.locateOnScreen("img/wait.png", confidence=0.8) #takes new screenshot
                 
-        elif abs(travel_x)>=abs(travel_y):
+                
+        elif abs(travel_x)>=abs(travel_y): #pathfinding if x coord higher than y, will click on x coords
             if travel_x>0:
                 hc.move((x_player-800,y_player),p)
                 pg.leftClick()
@@ -91,7 +79,7 @@ def giga_loop():
             else:
                 break
 
-        elif abs(travel_y)>abs(travel_x):
+        elif abs(travel_y)>abs(travel_x): #pathfinding if y coord higher than x, will click on y coords
             if travel_y>0:
                 hc.move((x_player,y_player-200),p)
                 pg.leftClick()
